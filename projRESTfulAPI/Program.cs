866737f5fe92aed018293e2e806fdcdef023d55a
YanLin <yan0988777698@gmail.com>
2024-05-23 14:42:07 +0800

    #設定CORS

--- projRESTfulAPI/Program.cs
+++ projRESTfulAPI/Program.cs
@@ -6,6 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<MyDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyDBConntectionString")));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -14,7 +19,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers().AddXmlDataContractSerializerFormatters();
 
 var app = builder.Build();
-
+app.UseCors("AllowAll");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
